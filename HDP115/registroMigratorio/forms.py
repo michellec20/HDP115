d8eada494525c3654a2705ed9e9d5a5b3088e389
michellec20 <cc18064@ues.edu.sv>
2022-06-19 00:52:09 -0600

    Configuraciones, templates, migrations

---
+++ HDP115/registroMigratorio/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import persona
+
+#Formulario para Crear Personas
+class PersonaForm(forms.ModelForm):
+   
+    class Meta:
+        model = persona
+        fields = ('nombre','apellido', 'paisOrigen', 'paisDestino', 'pasaporte', 'tiempoPermanencia')
+        label = {
+            
+            'nombre':('Nombre:'),
+            'apellido':('Apellido:'),
+            'paisOrigen':('Pais Origen:'),
+            'paisDestino':('Pais Destino:')
+            'pasaporte':('Pasaporte:'),
+            'tiempoPermanencia':('Tiempo de Permanencia:'),
+            
+        }
+        help_texts ={
+           
+            'nombre':('Campo obligatorio'),
+            'apellido':('Campo obligatorio'),
+            'paisOrigen':('Campo obligatorio'),
+            'paisDestino':('Campo obligatorio'),
+            'pasaporte':('Campo obligatorio'),
+            'tiempoPermanencia':('Campo obligatorio')
+            
+        }
+class PersonasSistema(forms.Form):
+    fields = {'pasaporte'}
---
+++ HDP115/registroMigratorio/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+nacional_extranjero= [
+    (1, 'Nacional'),
+    (2, 'Extranjero')
+]
+tipo_Documento=[
+    (1, 'DUI'),
+    (2, 'Pasaporte')
+]
+
+class persona(models.Model):
+    extrajero = models.ForeignKey('Extranjero', on_delete=models.CASCADE, null=True)
+    ciudadano = models.ForeignKey('Ciudadano', on_delete=models.CASCADE, null=True)
+    pasaporte = models.CharField(max_length=9, unique=True, default="", null=True)
+    dui = models.CharField(max_length=10, unique=True, default="", null=True)
+    nombre = models.CharField(max_length= 50, default="")
+    apellido = models.CharField(max_length=50, default="")
+    tipoDocumento = models.IntegerField(
+        blank=True,
+        choices=tipo_Documento
+    ) 
+    nacionalidad = models.IntegerField(
+        blank=True,
+        choices=nacional_extranjero
+    ) 
+    fechaSalida = models.DateField()
+    fechaIngreso = models.DateField()
+    TiempoPermanencia = models.CharField(max_length=10, default="")
+
+
+
+class Extranjero(models.Model):
+    idExtranjero = models.AutoField(primary_key=True)
+
+class Ciudadano(models.Model):
+    idCiudadano = models.AutoField(primary_key=True)
